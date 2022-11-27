b7b30424c3772123e79ff969e2c89cd8f90b67ab
桃夭 <z-taoyao@qq.com>
2022-11-27 13:30:20 +0800

    feat: - 集成uview ui 库

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # goframe-shop-mobile 移动端商城
 
 - 基于uniapp框架
+- 使用vue2版本
 - uview ui库
+
+## 启动方式
+
+0. 将项目导入Hbuilder中。
+1. 点击运行到所要运行的平台。
+
+## 
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import App from './App'
 
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
@@ -8,14 +11,4 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
 // #endif
