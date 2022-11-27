26e234e085b6713bd64fb64c4a1067d5746a380a
桃夭 <z-taoyao@qq.com>
2022-11-27 13:53:31 +0800

    feat: - add global variable

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ import App from './App'
 import uView from '@/uni_modules/uview-ui'
 Vue.use(uView)
 
+
+import store from '@/store';
+
+
+let vuexStore = require("@/store/$u.mixin.js");
+Vue.mixin(vuexStore);
+
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
+	store,
     ...App
 })
 app.$mount()
