abb132f5de1a049e29ed41f46501ecd36a25ec48
桃夭 <z-taoyao@qq.com>
2022-11-27 18:54:31 +0800

    style: - 修改home页面样式,添加商品标签

--- components/Goods/GoodsItem.vue
+++ components/Goods/GoodsItem.vue
@@ -1,12 +1,6 @@
 <template>
 	<view class="">
 		<view>
-			<!-- <u-row justify="space-between" gutter="10"  v-for="(item,index) in goodsList" :key="item.id">
-				<u-col span="6">
-					
-				</u-col>
-			</u-row> -->
-			
 			<view class="goods-view">
 				<view class="goods-card" v-for="(item,index) in goodsList" :key="item.id">
 					<!-- 图片 -->
@@ -15,19 +9,24 @@
 					<view class="">
 						<u--text :lines="2" :text="item.name"></u--text>
 					</view>
-					<!-- 价格 -->
-					<!-- 现价 -->
-					<!-- 划线价格 -->
-					<view class="goods-bottom">
-						<view class="goods-price">
-							<u--text mode="price" :text="item.price" color="#ff0000"></u--text>
-							<u--text mode="price" :text="item.showOriginalPrice" color="#909399" size="10" decoration="line-through"></u--text>
+					<view class="">
+						<view class="goods-tag">
+							<view class="goods-tag-text">标签</view>
+							<view class="goods-tag-text">标签</view>
 						</view>
-						<!-- 立即购买-加入购物车 -->
-						<view class="">
-							<u--text mode="text" text="加入购物车" size="14" color="#ff0000"></u--text>
+						<view class="goods-bottom">
+							<view class="goods-price">
+								<!-- 价格 -->
+								<u--text mode="price" :text="item.price" color="#ff0000" size="18"></u--text>
+							</view>
+							<!-- 立即购买-加入购物车 -->
+							<view class="goods-cart">
+								<u--text mode="text" text="加入购物车" size="14" color="#ff0000"></u--text>
+							</view>
 						</view>
 					</view>
+
+
 				</view>
 			</view>
 
@@ -81,22 +80,36 @@
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
+
 		.goods-card {
-			margin:10rpx;
+			margin: 10rpx;
 			padding: 10rpx;
-			width: 330rpx;	
+			width: 330rpx;
 			// flex-grow: 1;
 			// border: 1rpx solid #c8c9cc;
 			box-shadow: 0 4rpx 8rpx 0 rgba(0, 0, 0, 0.1), 0 6rpx 20rpx 0 rgba(0, 0, 0, 0.09);
-			
-			.goods-price{
+
+			.goods-price {
 				// display: flex;
 			}
-			
-			.goods-bottom{
+
+			.goods-bottom {
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
+			}
+
+			.goods-tag {
+				display: flex;
+
+				.goods-tag-text {
+					padding: 5rpx;
+					margin: 2rpx;
+					font-size: 15rpx;
+					border: 2rpx solid #bdb9b9;
+					border-radius: 10rpx;
+					color: #bdb9b9;
+				}
 			}
 		}
 	}
